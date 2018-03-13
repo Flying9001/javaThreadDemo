a409de007ff4e7e8632092c3c6abb1403d5848f8
flying9001 <flying9001@gmail.com>
2018-03-13 22:09:15 +0800

    添加多线程同步测试

---
+++ src/main/java/com/ljq/demo/ThreadsyncDemo.java
@@ -0,0 +1,43 @@
+package com.ljq.demo;
+
+import com.ljq.utils.SyncTask;
+
+/**
+ * @description: 线程同步测试
+ * @author: lujunqiang
+ * @email: flying9001@gmail.com
+ * @date: 2018/3/13
+ */
+public class ThreadsyncDemo {
+
+
+    /**
+     * 当一个线程访问object的一个synchronized同步代码块或同步方法时，其他线程对object中所有其它synchronized
+     * 同步代码块或同步方法的访问将被阻塞
+     * */
+    public static void main(String[] args) {
+
+        SyncTask syncTask = new SyncTask();
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                syncTask.doTask1();
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                syncTask.doTask2();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+
+    }
+
+
+}
---
+++ src/main/java/com/ljq/utils/SyncTask.java
@@ -0,0 +1,31 @@
+package com.ljq.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @description: 同步线程
+ * @author: lujunqiang
+ * @email: flying9001@gmail.com
+ * @date: 2018/3/13
+ */
+public class SyncTask {
+
+    private static final Logger logback = LoggerFactory.getLogger(SyncTask.class);
+
+    public synchronized void doTask1(){
+
+        for (int i = 0; i < 10; i++) {
+            logback.debug("doTask1 is running " + i);
+        }
+    }
+
+    public synchronized void doTask2(){
+
+        for (int i = 0; i < 10; i++) {
+            logback.debug("doTask2 is running " + i);
+        }
+    }
+
+
+}
